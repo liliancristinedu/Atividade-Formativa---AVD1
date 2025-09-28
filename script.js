e32450efe4234e8b85be3ed17037c2b8a9c5c193
liliancristinedu <liliancdl123@gmail.com>
2025-09-28 11:32:19 -0300

    adição get element by id

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form-endereco');
+const cepInput = document.getElementById('cep');
+const logradouroInput = document.getElementById('logradouro');
+const numeroInput = document.getElementById('numero');
+const ufInput = document.getElementById('uf');
+
+
+
